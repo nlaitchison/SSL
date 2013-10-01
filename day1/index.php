bd94a2a3d3494bfc1828295b470b9f13b343a913
Nicole Aitchison <nlaitchison@gmail.com>
2013-10-01 16:06:51 -0400

    moved nav to nav.php

--- day1/index.php
+++ day1/index.php
@@ -1,4 +1,5 @@
 <? include 'views/header.php'; ?>
+<? include 'views/nav.php'; ?>
 <? include 'views/body.php'; ?>
 <? include 'views/footer.php'; ?>
 
--- day1/views/header.php
+++ day1/views/header.php
@@ -25,15 +25,3 @@
 				</form>
 
 				<div class="clear_fix"></div>
-
-				<nav>
-					<ul>
-						<li>All Albums</li>
-						<li>New Albums</li>
-						<li>Used Albums</li>
-						<li>Other Stuff</li>
-						<li>Questions</li>
-					</ul>
-				</nav>
-				<div class="clear_fix"></div>
-			</header>
